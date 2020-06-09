e2b413bbeaf30ddf1af47425a842a86fe2c60ff5
cjyojcomo <2070319400101@ingenieria.usac.edu.gt>
2020-06-09 12:25:29 -0600

    Colletz con mi numero de carnet

--- corto1/corto1.py
+++ corto1/corto1.py
@@ -1,12 +1,20 @@
-collatz= int(input("Ingresa un numero: "))
+lista=[]
+def corto(collatz):
 
-while collatz !=1:
-    if collatz%2==0:
-        print("%d," % (collatz), end="")
-        collatz = collatz/2
-    else:
-        print("%d," % (collatz), end="") 
-        collatz=(collatz*3)+1
+    while collatz !=1:
+        if collatz%2==0:
+            #lista[].append(collatz)
+            print("%d," % (collatz), end="")
+            collatz = collatz/2
+        else:
+            print("%d," % (collatz), end="") 
+            collatz=(collatz*3)+1
 
-    if collatz == 1:
-            print("1")      
+        if collatz == 1:
+            print("1")
+            
+for i in range(2,779):
+    corto(i)
+    #print lista[0]
+
+                    
