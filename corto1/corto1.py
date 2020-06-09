b7692a3a9477a69b01e5e78f71074b5dc90f4603
cjyojcomo <2070319400101@ingenieria.usac.edu.gt>
2020-06-09 12:34:30 -0600

    Comentarios y txt

--- corto1/corto1.py
+++ corto1/corto1.py
@@ -1,20 +1,29 @@
+#Se crea una lista
 lista=[]
+#Creo la funcion corto que ejecutara el codigo de collatz
 def corto(collatz):
 
+    #El codigo se ejecuta siempre y cuando no sea 1
     while collatz !=1:
+        #si es par se divide en dos
         if collatz%2==0:
-            #lista[].append(collatz)
+            lista.append(collatz)
             print("%d," % (collatz), end="")
             collatz = collatz/2
+        #Si no es par se multiplica por 3 y se le suma 1
         else:
             print("%d," % (collatz), end="") 
             collatz=(collatz*3)+1
+            lista.append(collatz)
 
+        @Si es uno se imprime el uno y se termina 
         if collatz == 1:
+            lista.append("1")
             print("1")
             
+#Creo un ciclo para que se ejecute desde el 2 hasta mi numero de carnet
 for i in range(2,779):
     corto(i)
-    #print lista[0]
+    #print (lista[35])
 
                     
